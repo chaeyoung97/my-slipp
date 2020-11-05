6f01c9f4d52cacb3a33292297bdee08a513802da
chaeyoung <codud7239@gmail.com>
2020-11-06 04:59:51 +0900

    회원목록구현

---
+++ src/main/java/net/slipp/web/User.java
@@ -0,0 +1,28 @@
+package net.slipp.web;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
