386450da285fe282efdeedb9612292025ecb89c2
chaeyoung <codud7239@gmail.com>
2020-11-18 03:49:35 +0900

    게시글 기본으로 db에 insert 하기

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) values ('cy9700', '1111', '채영', 'aaa@inha.ac');
 INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) values ('hy9600', '1111', '하영', 'bbb@inha.ac');
-insert into question (contents, title, writer) values ('첫번째 게시글', '첫번째', '채영');
-insert into question (contents, title, writer) values ('두번쨰 게시글', '두번쨰', '하영');
-insert into question (contents, title, writer) values ( '세번째 게시글', '세번째', '채영');
+insert into question (writer_id,contents, title, create_date) values (1,'첫번째 게시글', '첫번째',CURRENT_TIMESTAMP());
+insert into question (writer_id,contents, title, create_date) values (2,'두번째 게시글', '두번째',CURRENT_TIMESTAMP());
+insert into question (writer_id,contents, title, create_date) values (1,'세번째 게시글', '세번째',CURRENT_TIMESTAMP());
